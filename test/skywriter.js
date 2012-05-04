95c1c8996f5339937f6ba4c2308ee049294dbfe0
Paul O'Fallon <paul@ofallonfamily.com>
2012-05-04 18:50:09 -0400

    Removed unused test

--- test/skywriter.js
+++
@@ -1,4 +0,0 @@
-var path = process.env.HOME || (process.env.HOMEDRIVE + process.env.HOMEPATH);
-var testCredentials = JSON.parse(fs.readFileSync(path + '/.bluesky/test.json','ascii'));
-
-var storage = require('bluesky').storage({account: testCredentials.account, key: testCredentials.key});
